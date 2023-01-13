a95ceb443ab5671b7f6e3b11f8716abc2e1e760b
jumosd <jumosd@icloud.com>
2023-01-13 18:07:31 +0900

    2

--- webtoon/models.py
+++ webtoon/models.py
@@ -8,4 +8,4 @@ class Webtoon(models.Model):
     title = models.CharField(max_length=20)
     genre = models.CharField(max_length=20)
     tag = models.CharField(max_length=20)
-    thumbnail = models.CharField(max_length=20)
+    thumbnail = models.ImageField()
